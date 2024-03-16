47a3b3117ad63e2460b36d07c566e8c925d3d871
Ivan <radisav80@yahoo.com>
2024-03-16 18:57:20 +0100

    settings.py changed

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
         'NAME': 'realestate_db',
         'USER': 'postgres',
         'PASSWORD': 'sanantonio021',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'HOST': 'localhost',
+        
     }
 }
 
