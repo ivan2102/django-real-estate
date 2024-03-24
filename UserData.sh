76bb56a542827d9d70c741a18cb7535c9a3b4478
Ivan <radisav80@yahoo.com>
2024-03-25 00:17:40 +0100

    2 changes added

--- UserData.sh
+++ UserData.sh
@@ -4,9 +4,10 @@ set -e
 
 GIT_REPO_URL="https://github.com/ivan2102/django-real-estate.git"
 
+# If using Private Repo
+#GIT_REPO_URL="https://<your_username>:<your_PAT>@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
 
-
-
+# Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
 PROJECT_MAIN_DIR_NAME="django_real_estate"
 
 # Clone repository
@@ -23,3 +24,5 @@ chmod +x scripts/*.sh
 ./scripts/gunicorn.sh
 ./scripts/nginx.sh
 ./scripts/start_app.sh
+
+
