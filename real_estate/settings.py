8a1d7ae97ec258748f5107e604f393a0502923e2
Ivan <radisav80@yahoo.com>
2024-04-07 21:57:02 +0200

    Added local settings import

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -159,3 +159,10 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='iradisavljevic168@gmail.com'
 EMAIL_HOST_PASSWORD='wnev ohmp hlpi vrar'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+ 
